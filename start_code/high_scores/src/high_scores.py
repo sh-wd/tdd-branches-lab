a2c3b439f0cd7b9d7eb861b71addfb5703a40334
beckystorch <user@users-MacBook-Pro-10.local>
2023-03-31 10:38:47 +0100

    finishing high scores task

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -7,5 +7,10 @@ def personal_best(scores):
 
 
 def personal_top_three(scores):
-    sorted_scores = sorted(scores, reverse=True)
+    sorted_scores = order_highest_to_lowest(scores)
     return sorted_scores[0:3]
+
+
+def order_highest_to_lowest(scores):
+    sorted_scores = sorted(scores, reverse=True)
+    return sorted_scores
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -32,10 +32,34 @@ class HighScoresTest(unittest.TestCase):
 
 
     # Test ordered from highest tp lowest
+    # def test_ordered_highest_to_lowest(self):
+    #     expected = [10, 5 ,4, 2, 2]
+    #     actual_value = order_highest_to_lowest(self.scores)
+    #     self.assertEqual(expected, actual_value)
+
 
     # Test top three when there is a tie
+    def test_can_get_top_3_when_tied(self):
+        scores = [2, 4, 10, 2, 5, 5]
+        expected = [10, 5, 5]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected, actual_value)
+
+
+
 
     # Test top three when there are less than three
+    def test_get_top_3_when_less_than_3(self):
+        scores = [1, 2]
+        expected = [2, 1]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected, actual_value)
+
 
     # Test top three when there is only one
     
+    def test_get_top_3_when_less_than_1(self):
+        scores = [1]
+        expected = [1]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected, actual_value)
